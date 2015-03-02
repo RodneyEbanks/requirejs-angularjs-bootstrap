9eab5a7f564d9b965677ca4aa223e09c7dc3cc04
rodneyebanks <rodney@rodneyebanks.com>
2015-03-02 01:42:04 -0500

    Added manual angular.bootstrap if ng-app attribute not included in html page.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,14 +5,13 @@
  * Available via the MIT or new BSD license.
  */
 
+// Deferring Angular Bootstrap until we have loaded the necessary modules
+// http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = 'NG_DEFER_BOOTSTRAP!';
 
 define(['config!paths', 'angular'], function($config) {
-    // Deferring Angular Bootstrap until we have loaded the necessary modules
-    // http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 
-
-    // Function return from AMD require call;
+    // Function returned from AMD require call;
     return function bootstrap(filter, config) {
         var $module = window.angular.module,
             modules = config || $config || {},
@@ -34,10 +33,15 @@ define(['config!paths', 'angular'], function($config) {
 
         // Starting bootstrap by loading all required AMD/Shimmed scripts which should load Angular modules
         requirejs(requireModules, function(domReady) {
-            // Starting Angular main module on domReady or deviceReady (if running on Cordova);
             // RequireJS domReady function this function is called once the DOM is ready.
             //It will be safe to query the DOM and manipulate DOM nodes in this function.
             domReady(function() {
+
+                // Starting Angular bootstrap if ng-app directive not in html page;
+                if(!window.angular.resumeBootstrap) {
+                   window.angular.bootstrap(document, []);
+                }
+
                 // Loader to resume Angular Bootstrap when we are ready
                 function angularBootstrap() {
                     window.angular.resumeBootstrap(angularModules);
